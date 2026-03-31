a0b51e17fdcc832e6d124b10ccff18debb03cac1
Bertrand Moty <Bertrand Mot>
2026-03-31 15:35:32 +0200

    Premier visuel de Respawn

---
+++ Respawn/src/Controller/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')] // La route "/" signifie que c'est ta page d'accueil
+    public function index(): Response
+    {
+        // On demande à Symfony d'afficher le template du forum Gaming
+        // Assure-toi que le fichier existe dans templates/accueil/index.html.twig 
+        // ou adapte le chemin ci-dessous :
+        return $this->render('accueil/index.html.twig');
+    }
+}
